e6847520aa2abedd72e4810cf5c20f51e8aa2df5
Amitava <dattaarup2004@gmail.com>
2025-04-19 14:05:22 +0530

    vercel analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -8,6 +7,7 @@ import Index from "./pages/Index";
 import Gallery from "./pages/Gallery";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./hooks/useTheme";
+import { Analytics } from '@vercel/analytics/react'; // <-- Imported Analytics
 
 const queryClient = new QueryClient();
 
@@ -25,6 +25,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics /> {/* <-- Added Analytics component here */}
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
